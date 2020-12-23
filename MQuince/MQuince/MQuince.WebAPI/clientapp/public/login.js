cb836ca3fa12d56484712692e296c45bb38d28d7
Helena Anisic <helena.anisic@gmail.com>
2020-12-23 22:01:09 +0100

    Implement patient registration feature frontend

---
+++ MQuince/MQuince/MQuince.WebAPI/clientapp/public/login.js
@@ -0,0 +1,27 @@
+﻿var app = new Vue({
+    el: '#login',
+    data: {
+        email: "",
+        password:""
+    },
+    methods: {
+        submit: function () {
+            axios
+                .get('/api/Patient/Login',
+                    {
+                        Email: this.email,
+                        Password: this.password
+                    })
+                .then(response => {
+                    this.info = response.data
+                    JSAlert.alert("You have successfully logged into your account!");
+                })
+                .catch(error => {
+                    console.log(error);
+                    if (error.response.status == 400) {
+                        JSAlert.alert("Password or email address are incorrect. Please try again.");
+                    }
+                })
+        })
+    }
+})
