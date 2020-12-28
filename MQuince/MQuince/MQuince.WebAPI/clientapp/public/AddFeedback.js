f8fe1225196f2c5c71d1cee9ab9a0ae45f64e4bf
Helena Anisic <helena.anisic@gmail.com>
2020-12-28 11:18:09 +0100

    implement patient create feedback feature frontend

---
+++ MQuince/MQuince/MQuince.WebAPI/clientapp/public/AddFeedback.js
@@ -0,0 +1,31 @@
+﻿
+var app = new Vue({
+	el: '#addFeedback',
+	data: {
+		Comment: ""
+	},
+	methods: {
+		submit() {
+			if (this.Comment != "") {
+				axios
+					.post("/api/Feedback", {
+						Comment: this.Comment
+					}).then(response => {
+						JSAlert.alert("Your feedback has been saved!");
+
+						setTimeout(function () {
+							if (window.location.hash != '#r') {
+								window.location.hash = 'r';
+								window.location.reload(1);
+							}
+						}, 3000);
+
+
+					})
+			} else {
+				JSAlert.alert("You have to fill in the form");
+			}
+
+		}
+	}
+})
