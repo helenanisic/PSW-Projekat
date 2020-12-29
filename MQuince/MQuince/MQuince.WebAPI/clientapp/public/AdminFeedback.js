cb83a95c52c682a6a0e6270f174bd3852c556b4f
Helena Anisic <helena.anisic@gmail.com>
2020-12-29 12:05:32 +0100

    implement admin publish feedbacks feature frontend

---
+++ MQuince/MQuince/MQuince.WebAPI/clientapp/public/AdminFeedback.js
@@ -0,0 +1,34 @@
+﻿var app = new Vue({
+	el: '#adminFeedback',
+	data: {
+		status: "Published",
+		feedbacks: []
+	},
+	methods: {
+		statusChanged() {
+			if (this.status == "Published") {
+				axios
+					.get('/api/Feedback/GetPublishedFeedbacks')
+                    .then(response => {
+						this.feedbacks = response.data
+					})
+			}
+			else (this.status == "Pending") {
+				axios
+					.get('/api/Feedback/GetNotPublishedFeedbacks')
+                    .then(response => {
+						this.feedbacks = response.data
+					})
+			}
+			
+		},
+    },
+	created() {
+		axios
+			.get('/api/Feedback/GetPublishedFeedbacks')
+            .then(response => {
+				this.feedbacks = response.data
+			})
+
+    }
+})
---
+++ MQuince/MQuince/MQuince.WebAPI/clientapp/public/Feedbacks.js
@@ -0,0 +1,17 @@
+﻿var app = new Vue({
+	el: '#feedbacks',
+	data: {
+		feedbacks: []
+	},
+	methods: {
+
+	},
+	created() {
+		axios
+			.get('/api/Feedback/GetPublishedFeedbacks')
+            .then(response => {
+				this.feedbacks = response.data
+			})
+
+	}
+})
