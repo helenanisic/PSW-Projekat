7be772652c52d1fbec2449de5e43dc4860b695b1
helenanisic <58892678+helenanisic@users.noreply.github.com>
2021-06-24 18:20:16 +0200

    implement malicious-patients-list frontend

---
+++ MQuince/MQuince/MQuince.WebAPI/clientapp/public/PatientBan.js
@@ -0,0 +1,21 @@
+﻿var app = new Vue({
+	el: '#patientBan',
+	data: {
+		appointments: []
+	},
+	methods: {
+
+	},
+	created() {
+		axios
+			.get('/api/Appointment/GetAppointmentsForPatient', {
+				headers: {
+					'Authorization': "Bearer " + localStorage.getItem("access_token")
+				}
+			})
+			.then(response => {
+				this.appointments = response.data
+			})
+
+	}
+})
