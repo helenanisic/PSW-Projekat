fadadea469a88b89245564b6841db48f5e04c6b0
helenanisic <58892678+helenanisic@users.noreply.github.com>
2021-06-29 12:13:07 +0200

    implement order frontend

---
+++ MQuince/MQuince/MQuince.WebAPI/Controllers/OrderController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MQuince.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> CreateErrand()
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            using var channel = GrpcChannel.ForAddress("http://localhost:9001");
+            var client = new ErrandService.ErrandServiceClient(channel);
+            var response = await client.newErrandAsync(new ErrandRequest { Deadline = "2021-08-21" });
+            return Ok(response.Id);
+        }
+
+        [HttpGet("medications")]
+        public async Task<IActionResult> Medications()
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            using var channel = GrpcChannel.ForAddress("http://localhost:9001");
+            var client = new ErrandService.ErrandServiceClient(channel);
+            ErrandMedicationRequest errandMedicationRequest = new ErrandMedicationRequest();
+            errandMedicationRequest.ErrandMedications.Add(new ErrandMedication() { ErrandId = 1, Id = 2, Name = "Metafex", Quantity = 1});
+            var response = await client.errandMedicationAsync(errandMedicationRequest);
+            return Ok(response.Response);
+        }
+    }
+}
